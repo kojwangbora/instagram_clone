eb36a76a50633a66fb6a69188c5f6acda8dcf5ee
kojwangbora <kojwangbora254@gmail.com>
2022-04-04 00:25:51 +0300

    create logut fn

--- insta/urls.py
+++ insta/urls.py
@@ -1,6 +1,8 @@
-from django.urls import re_path
+from django.urls import re_path, path,include
 from . import views
 
 urlpatterns=[
-    re_path('',views.welcome,name = 'welcome'),
+    re_path('signup/',views.signup,name = 'signup'),
+   path('account/', include('django.contrib.auth.urls')),
+    path('', views.index, name='index')
 ]
--- insta/views.py
+++ insta/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.http  import HttpResponse
 from .forms import SignUpForm, UpdateUserForm, UpdateUserProfileForm, PostForm, CommentForm
 from django.contrib.auth import login, authenticate
