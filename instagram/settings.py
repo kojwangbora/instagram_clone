15034737d8b4cb8ff62d0c50328ad6126498348e
kojwangbora <kojwangbora254@gmail.com>
2022-04-03 22:55:11 +0300

    add login and signup tem files

--- instagram/settings.py
+++ instagram/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'insta',
     'bootstrap4',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
