9bba45ddfc2582715c3cc935bca5bac9018ba179
kojwangbora <kojwangbora254@gmail.com>
2022-04-04 09:52:49 +0300

    solve errornotfoundmodule

--- instagram/settings.py
+++ instagram/settings.py
@@ -32,14 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'insta',
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'insta',
+     'crispy_forms',
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
