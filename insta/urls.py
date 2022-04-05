55d4d8d30b166d7cdc8a04efcea48f2d6e94dbe1
kojwangbora <kojwangbora254@gmail.com>
2022-04-05 09:13:03 +0300

    solve routing blocker to make the landing page contain login and signup htmls

--- insta/urls.py
+++ insta/urls.py
@@ -1,10 +1,13 @@
 from django.urls import re_path, path,include
 from . import views
+from django.contrib.auth.views import LoginView
+# from django.contrib.auth import authenticate, login
 
 urlpatterns=[
-    re_path('signup/',views.signup,name = 'signup'),
-   path('account/', include('django.contrib.auth.urls')),
-    path('', views.index, name='index'),
+    re_path('signup',views.signup,name = 'signup'),
+    path('account/', include('django.contrib.auth.urls')),
+    re_path(r'', LoginView.as_view(), {"next_page": '/'}),
+    # path('', views.index, name='index'),
     path('search/', views.search_profile, name='search'),
     # path('profile/<username>/', views.profile, name='profile'),
     # path('user_profile/<username>/', views.user_profile, name='user_profile'),
